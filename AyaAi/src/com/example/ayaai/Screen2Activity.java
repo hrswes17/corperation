5689ada71e12a837ada071b5cd957e0e542ab754
quality-leaf <oh.ayaka.ballpen@gmail.com>
2013-08-31 06:11:27 +0900

    add the screen3

--- AyaAi/src/com/example/ayaai/Screen2Activity.java
+++ AyaAi/src/com/example/ayaai/Screen2Activity.java
@@ -1,13 +1,41 @@
 package com.example.ayaai;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Screen2Activity extends Activity {
+public class Screen2Activity extends Activity implements OnClickListener{
+	private Button bt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_screen2);
+		
+		bt = (Button)findViewById(R.id.to_screen3button);
+		bt.setOnClickListener(this);
+		Log.v("onCreate","testestes");
+	}
+
+	
+	public boolean onCreateOptionsMenu(Menu menu){
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		if(v==bt){
+			Log.v("test!","testes");
+			Intent intent_scr3 = new Intent(this,Screen3Activity.class);
+			startActivity(intent_scr3);
+		}
+		else{Log.v("else","else");}
 		
 	}
 
