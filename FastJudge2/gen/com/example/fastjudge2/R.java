6052006420ad7cd9c00607190d26f39ea120385d
hrswes17 <hrswes.17@gmail.com>
2013-09-10 22:19:17 +0900

    fastjudge2

--- FastJudge2/gen/com/example/fastjudge2/R.java
+++ FastJudge2/gen/com/example/fastjudge2/R.java
@@ -24,7 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000d;
+        public static final int button1=0x7f080002;
+        public static final int button10=0x7f08000b;
+        public static final int button11=0x7f08000a;
+        public static final int button12=0x7f08000c;
+        public static final int button2=0x7f080001;
+        public static final int button3=0x7f080003;
+        public static final int button4=0x7f080005;
+        public static final int button5=0x7f080004;
+        public static final int button6=0x7f080006;
+        public static final int button7=0x7f080008;
+        public static final int button8=0x7f080007;
+        public static final int button9=0x7f080009;
+        public static final int quest_text=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +47,43 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int anything=0x7f05000e;
         public static final int app_name=0x7f050000;
+        public static final int back=0x7f050003;
+        public static final int boy_girl=0x7f050015;
+        public static final int cafe=0x7f050007;
+        public static final int dinner=0x7f05000b;
+        public static final int family=0x7f050018;
+        public static final int famires=0x7f05000a;
+        public static final int feel1=0x7f05001b;
+        public static final int feel10=0x7f050024;
+        public static final int feel11=0x7f050025;
+        public static final int feel2=0x7f05001c;
+        public static final int feel3=0x7f05001d;
+        public static final int feel4=0x7f05001e;
+        public static final int feel5=0x7f05001f;
+        public static final int feel6=0x7f050020;
+        public static final int feel7=0x7f050021;
+        public static final int feel8=0x7f050022;
+        public static final int feel9=0x7f050023;
+        public static final int friend=0x7f050014;
         public static final int hello_world=0x7f050002;
+        public static final int home=0x7f050010;
+        public static final int lunch=0x7f050009;
+        public static final int many=0x7f050016;
+        public static final int meet=0x7f050005;
+        public static final int not_friend=0x7f050019;
+        public static final int now_feeling=0x7f05001a;
+        public static final int one=0x7f050013;
+        public static final int out=0x7f050011;
+        public static final int party=0x7f050006;
+        public static final int pastapizza=0x7f05000d;
+        public static final int ra_men=0x7f050008;
+        public static final int seafood=0x7f05000c;
+        public static final int senkou=0x7f050017;
+        public static final int what_keyword=0x7f050004;
+        public static final int where_food=0x7f05000f;
+        public static final int who_food=0x7f050012;
     }
     public static final class style {
         /** 
--- FastJudge2/src/com/example/fastjudge2/MainActivity.java
+++ FastJudge2/src/com/example/fastjudge2/MainActivity.java
@@ -2,14 +2,82 @@ package com.example.fastjudge2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+	
+	private String search_word="";
+	
+	private Button bt1;
+	private Button bt2;
+	private Button bt3;
+	private Button bt4;
+	private Button bt5;
+	private Button bt6;
+	private Button bt7;
+	private Button bt8;
+	private Button bt9;
+	private Button bt10;
+	private Button bt11;
+	private Button bt12;
+	private TextView question;
 
+	Intent next_intent;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		question=(TextView)findViewById(R.id.quest_text);
+		question.setText(R.string.now_feeling);
+		
+		bt1=(Button)findViewById(R.id.button1);
+		bt2=(Button)findViewById(R.id.button2);
+		bt3=(Button)findViewById(R.id.button3);
+		bt4=(Button)findViewById(R.id.button4);
+		bt5=(Button)findViewById(R.id.button5);
+		bt6=(Button)findViewById(R.id.button6);
+		bt7=(Button)findViewById(R.id.button7);
+		bt8=(Button)findViewById(R.id.button8);
+		bt9=(Button)findViewById(R.id.button9);
+		bt10=(Button)findViewById(R.id.button10);
+		bt11=(Button)findViewById(R.id.button11);
+		bt12=(Button)findViewById(R.id.button12);
+			
+		
+		bt1.setText(R.string.feel1);
+		bt2.setText(R.string.feel2);
+		bt3.setText(R.string.feel3);
+		bt4.setText(R.string.feel4);
+		bt5.setText(R.string.feel5);
+		bt6.setText(R.string.feel6);
+		bt7.setText(R.string.feel7);
+		bt8.setText(R.string.feel8);
+		bt9.setText(R.string.feel9);
+		bt10.setText(R.string.feel10);
+		bt11.setText(R.string.feel11);
+		bt12.setText(R.string.back);
+		
+		
+		bt1.setOnClickListener(this);		
+		bt2.setOnClickListener(this);		
+		bt3.setOnClickListener(this);		
+		bt4.setOnClickListener(this);		
+		bt5.setOnClickListener(this);		
+		bt6.setOnClickListener(this);		
+		bt7.setOnClickListener(this);		
+		bt8.setOnClickListener(this);		
+		bt9.setOnClickListener(this);		
+		bt10.setOnClickListener(this);		
+		bt11.setOnClickListener(this);		
+		bt12.setOnClickListener(this);
+		
 	}
 
 	@Override
@@ -17,6 +85,55 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public void onClick(View view){
+		
+		next_intent = new Intent(this,FoodQuest2.class);
+		
+		if(view==bt1){
+			search_word+="お祝い";
+		}
+		else if(view==bt2){
+			search_word+="明るい";
+		}
+		else if(view==bt3){
+			search_word+="癒やし";
+			finish();
+		}
+		else if(view==bt4){
+			search_word+="癒やし";
+		}
+		else if(view==bt5){
+			search_word+="おもしろい";
+		}
+		else if(view==bt6){
+			search_word+="癒やし";
+		}
+		else if(view==bt7){
+			search_word+="明るい";
+		}
+		else if(view==bt8){
+			search_word+="";//あとで "こわい"
+		}
+		else if(view==bt9){
+			search_word+="おもしろい";
+		}
+		else if(view==bt10){
+			search_word+="きれい";
+		}
+		else if(view==bt11){//ほか
+			search_word+="";
+		}
+		else{//bt12...戻るボタン 一個前の情報(空白区切りで一単語消す)
+			search_word+="";
+		}
+	
+		startActivity(next_intent);
+		next_intent.putExtra("search", search_word);
+		startActivity(next_intent);
+//		finish();
 	}
 
 }
